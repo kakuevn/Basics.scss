7dbf16ed3898dd605461f04d5e6c8ebbf2e0470c
Nikita K <n.kakuev@gmail.com>
2019-04-03 20:48:01 +0100

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var clean = require("gulp-clean-css");
 
-gulp.task("sass", function() {
+gulp.task("build", function() {
   return gulp
     .src("./src/basics.scss")
     .pipe(sass().on("error", sass.logError))
